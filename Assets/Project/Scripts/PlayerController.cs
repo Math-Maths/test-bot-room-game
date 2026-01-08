00ca1d308df927b30758148ef1741ba4523cfc7e
Matheus <matheus_godoy97@hotmail.com>
2026-01-08 12:06:49 -0300

    feature: changed from rigidy body to character controller and created jump mechanic

--- Assets/Project/Scripts/PlayerController.cs
+++ Assets/Project/Scripts/PlayerController.cs
@@ -4,12 +4,16 @@ using UnityEngine.InputSystem;
 namespace TestBotRoom
 {
     [RequireComponent(typeof(PlayerInput))]
-    [RequireComponent(typeof(Rigidbody))]
+    [RequireComponent(typeof(CharacterController))]
     public class PlayerController : MonoBehaviour
     {
         
         private PlayerInput _playerInput;
-        private Rigidbody _playerRb;
+        private CharacterController _controller;
+
+        private float _gravityValue = -9.81f;
+        private bool _isGrounded;
+        private Vector3 _playerVelocity;
 
         #region Input Actions
         private InputAction _move;
@@ -18,11 +22,14 @@ namespace TestBotRoom
 
         [Header("Player Settings")]
         [SerializeField] private float moveSpeed;
+        [SerializeField] private float jumpForce;
+
+        private bool isGrounded;
 
         private void Awake()
         {
             _playerInput = GetComponent<PlayerInput>();
-            _playerRb = GetComponent<Rigidbody>();
+            _controller = GetComponent<CharacterController>();
 
             _move = _playerInput.actions["Move"];
             _jump = _playerInput.actions["Jump"];
@@ -30,16 +37,30 @@ namespace TestBotRoom
 
         private void Update()
         {
-            Vector2 rawMove = _move.ReadValue<Vector2>();
-            Vector3 velocity = new Vector3(rawMove.x, 0, rawMove.y) * moveSpeed * Time.deltaTime;
-            Vector3 moveDir = velocity.normalized;
+            _isGrounded = _controller.isGrounded;
 
-            _playerRb.transform.position += velocity;
-            
-            if (moveDir != Vector3.zero)
+            if(_isGrounded)
             {
-                _playerRb.transform.rotation = Quaternion.LookRotation(moveDir);
+                if(_playerVelocity.y < -2f)
+                    _playerVelocity.y = -2f;
             }
+
+            Vector2 moveInput = _move.ReadValue<Vector2>();
+            Vector3 move = new Vector3(moveInput.x, 0 , moveInput.y);
+            move = Vector3.ClampMagnitude(move, 1f);
+
+            if(move != Vector3.zero)
+                transform.forward = move;
+
+            if(_isGrounded && _jump.WasPressedThisFrame())
+            {
+                _playerVelocity.y = Mathf.Sqrt(jumpForce * -2f * _gravityValue);
+            }
+
+            _playerVelocity.y += _gravityValue * Time.deltaTime;
+
+            Vector3 finalMove = move * moveSpeed + Vector3.up * _playerVelocity.y;
+            _controller.Move(finalMove * Time.deltaTime);
         }
 
     }
