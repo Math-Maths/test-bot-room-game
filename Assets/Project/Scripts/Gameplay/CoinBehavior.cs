64c731a364909b2540cd3b4d32161068810d2bfa
Matheus <matheus_godoy97@hotmail.com>
2026-01-08 22:17:58 -0300

    feature: created the coin spawner and prefabs

---
+++ Assets/Project/Scripts/Gameplay/CoinBehavior.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace TestBotRoom
+{
+    public class CoinBehavior : MonoBehaviour
+    {
+        public event Action OnCoinColleted;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            OnCoinColleted?.Invoke();
+        }
+    }
+}
