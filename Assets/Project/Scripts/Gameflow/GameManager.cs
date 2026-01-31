52bcdfc0be3f92ddd2e021f8c2024dca634ba81f
Math-Maths <47351891+Math-Maths@users.noreply.github.com>
2026-01-31 16:59:45 -0300

    feature: made the GameManager able to use the data and also made a target frame rate

--- Assets/Project/Scripts/Gameflow/GameManager.cs
+++ Assets/Project/Scripts/Gameflow/GameManager.cs
@@ -9,6 +9,7 @@ namespace TestBotRoom
 
         private GameState _currentGameState;
         private DataManager _dataManager;
+        private GameStatus _gameStatus;
 
         public GameState CurrentGameState
         {
@@ -18,12 +19,18 @@ namespace TestBotRoom
 
         private void Awake()
         {
+            QualitySettings.vSyncCount = 0;
+            Application.targetFrameRate = 60;
+
             if(Instance == null) Instance = this;
             else Destroy(gameObject);
 
             DontDestroyOnLoad(gameObject); 
 
             _dataManager = GetComponent<DataManager>();
+            _gameStatus = new GameStatus();
+
+            LoadData();
         }
 
         private void OnEnable()
@@ -42,6 +49,35 @@ namespace TestBotRoom
             EventManager.Instance.Invoke(EventNameSaver.OnGameStarts);
         }
 
+        public void LoadData()
+        {
+            SaveData data = _dataManager.Load();
+
+            if(data == null)
+            {
+                _gameStatus = GameStatus.Default;
+                return;
+            }
+
+            _gameStatus.PlayerName = data.playerName;
+            _gameStatus.Coins = data.coins;
+            _gameStatus.Gears = data.gears;
+            _gameStatus.BestScore = data.bestScore;
+        }
+
+        public void SaveData()
+        {
+            SaveData data = new SaveData
+            {
+                bestScore = _gameStatus.BestScore,
+                coins = _gameStatus.Coins,
+                gears = _gameStatus.Gears,
+                playerName = _gameStatus.PlayerName
+            };
+
+            _dataManager.Save(data);
+        }
+
     }
 
     public enum GameState
@@ -50,5 +86,21 @@ namespace TestBotRoom
         Gameplay,
         Tutorial,
         GameOver
+    }
+
+    public class GameStatus
+    {
+        public static readonly GameStatus Default = new GameStatus
+        {
+            PlayerName = "Player",
+            Coins = 0,
+            Gears = 0,
+            BestScore = 0
+        };
+
+        public string PlayerName { get; set; }
+        public int Coins { get; set; }
+        public int Gears { get; set; }
+        public int BestScore { get; set; }
     }
 }
