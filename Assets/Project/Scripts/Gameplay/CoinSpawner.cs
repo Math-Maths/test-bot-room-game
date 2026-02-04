48720a5916821c81f7495ef7e4c612d879a2a9d1
Math-Maths <47351891+Math-Maths@users.noreply.github.com>
2026-02-03 21:09:41 -0300

    refactor: changed coin spawner to keep the flow correctly

--- Assets/Project/Scripts/Gameplay/CoinSpawner.cs
+++ Assets/Project/Scripts/Gameplay/CoinSpawner.cs
@@ -14,15 +14,23 @@ namespace TestBotRoom.Gameplay
         public void OnInitiate()
         {
             EventManager.Instance.AddListener(EventNameSaver.OnCoinColleted, ChangeCoinPosition);
+            EventManager.Instance.AddListener(EventNameSaver.OnGameOver, DisableCoin);
         }
 
         private void OnDisable()
         {
             EventManager.Instance.RemoveListener(EventNameSaver.OnCoinColleted, ChangeCoinPosition);
+            EventManager.Instance.RemoveListener(EventNameSaver.OnGameOver, DisableCoin);
         }
 
         public void CreateACoin()
         {
+            if(_currentCoin != null)
+            {
+                ChangeCoinPosition();
+                return;
+            }
+
             float randomX = Random.Range(-mapWidth, mapLenght);
             float randomZ = Random.Range(-mapLenght, mapLenght);
             Vector3 finalPosition = new Vector3(randomX, coinHeight, randomZ);
@@ -35,10 +43,18 @@ namespace TestBotRoom.Gameplay
             if(_currentCoin == null)
                 return;
 
+            _currentCoin.gameObject.SetActive(true);
+
             float randomX = Random.Range(-mapWidth, mapLenght);
             float randomZ = Random.Range(-mapLenght, mapLenght);
             _currentCoin.transform.position = new Vector3(randomX, _currentCoin.transform.position.y, randomZ);
             _currentCoin.SpawnAnimation();
+        }
+
+        private void DisableCoin()
+        {
+            if(_currentCoin != null)
+                _currentCoin.gameObject.SetActive(false);
         }
 
     }
