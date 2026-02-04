d42c0fe64ac0d332bf99fe6c24e39a19de59cb82
Math-Maths <47351891+Math-Maths@users.noreply.github.com>
2026-02-03 21:15:27 -0300

    feature: made a count down mechanic

--- Assets/Project/Scripts/UI/GameplayUIControl.cs
+++ Assets/Project/Scripts/UI/GameplayUIControl.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using System.Threading.Tasks;
 
 namespace TestBotRoom.UI
 {
@@ -8,21 +9,52 @@ namespace TestBotRoom.UI
         [Header("World UI Elements")]
         [SerializeField] private TMP_Text currentScoreText_World;
 
+        [Space(10)]
         [Header("End Screen UI Elements")]
         [SerializeField] private GameObject endScreenPanel;
         [SerializeField] private TMP_Text currentScoreText;
         [SerializeField] private TMP_Text bestScoreText;
 
+        [Space(10)]
+        [Header("UI Controls")]
+        [SerializeField] private GameObject controlsPanel;
+
+        [Space(10)]
+        [Header("Initial Gameplay Screen")]
+        [SerializeField] private TMP_Text startDelayText;
+
         public void OnInitiate()
         {
             EventManager.Instance.AddListener<int>(EventNameSaver.OnScoreChanged, UpdateScore);
             EventManager.Instance.AddListener<int>(EventNameSaver.OnBestScoreChanged, UpdateBestScore);
+            EventManager.Instance.AddListener(EventNameSaver.OnGameOver, DisableControls);
         }
 
         private void OnDisable()
         {
             EventManager.Instance.RemoveListener<int>(EventNameSaver.OnScoreChanged, UpdateScore);
             EventManager.Instance.RemoveListener<int>(EventNameSaver.OnBestScoreChanged, UpdateBestScore);
+            EventManager.Instance.RemoveListener(EventNameSaver.OnGameOver, DisableControls);
+        }
+
+        public async Task ShowCountdown(float seconds)
+        {
+            startDelayText.gameObject.SetActive(true);
+
+            int remaining = Mathf.CeilToInt(seconds);
+
+            while (remaining > 0)
+            {
+                startDelayText.text = remaining.ToString();
+                await Task.Delay(1000);
+                remaining--;
+            }
+
+            startDelayText.text = "GO!";
+            await Task.Delay(500);
+
+            startDelayText.gameObject.SetActive(false);
+            controlsPanel.SetActive(true);
         }
 
         private void UpdateScore(int value)
@@ -44,6 +76,11 @@ namespace TestBotRoom.UI
         {
             currentScoreText.text = finalScore.ToString("000");
             endScreenPanel.SetActive(true);
+        }
+
+        public void DisableControls()
+        {
+            controlsPanel.SetActive(false);
         }
     }
 }
