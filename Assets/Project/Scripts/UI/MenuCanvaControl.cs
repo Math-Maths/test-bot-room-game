d04f01f4e54277c1f6d31a5b969ed13106739ce9
Math-Maths <47351891+Math-Maths@users.noreply.github.com>
2026-02-18 12:44:28 -0300

    refactor: removed gears for now and unused script

--- Assets/Project/Scripts/UI/MenuCanvaControl.cs
+++ Assets/Project/Scripts/UI/MenuCanvaControl.cs
@@ -5,14 +5,14 @@ namespace TestBotRoom.UI
 {
     public class MenuCanvasControl : MonoBehaviour
     {
-        [SerializeField] private TMP_Text gearCountText;
+        //[SerializeField] private TMP_Text gearCountText;
         [SerializeField] private TMP_Text coinCountText;
         [SerializeField] private TMP_Text bestScoreText;
         [SerializeField] private TMP_Text playerNameText;
 
         public void Initialize(GameStatus playerData)
         {
-            gearCountText.text = playerData.Gears.ToString();
+            //gearCountText.text = playerData.Gears.ToString();
             coinCountText.text = playerData.Coins.ToString();
             bestScoreText.text = "Best Score: " + playerData.BestScore.ToString();
             playerNameText.text = playerData.PlayerName;
