dd84a30f857a643018b1818a52b9a1c119d5f18b
Math_Maths <matheus_godoy97@hotmail.com>
2026-04-10 22:19:44 -0300

    debug: added some debug to see the time of the next animation

--- Assets/OnFinish.cs
+++ Assets/OnFinish.cs
@@ -9,7 +9,7 @@ public class OnFinish : StateMachineBehaviour
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //Debug.Log(stateInfo.length);
+        //Debug.LogWarning($"The animation '{animationName}' has finished with a duration of {stateInfo.length} seconds.");
         animator.GetComponentInParent<PlayerController>().ShotAnimation(animationName, stateInfo.length);
     }
 }
