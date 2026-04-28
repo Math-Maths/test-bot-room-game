52fb6a4c8c40f1b608203b9f263e9543fe0941b5
Math_Maths <matheus_godoy97@hotmail.com>
2026-04-28 13:30:25 -0300

    chore: build new bindings

--- TestRoomCloud/Project/obj/Release/net6.0/linux-x64/TestRoomCloud.AssemblyInfo.cs
+++ TestRoomCloud/Project/obj/Release/net6.0/linux-x64/TestRoomCloud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestRoomCloud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd84a30f857a643018b1818a52b9a1c119d5f18b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22914aa53bda4a24db0a9ffa1f58630da18316d2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestRoomCloud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestRoomCloud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
